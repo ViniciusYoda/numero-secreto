dc9ad49eaa3ad4a22154688682c22f86ed178650
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-01 12:27:50 -0300

    reconhecimento de voz

---
+++ js/reconhecimentoDeVoz.js
@@ -0,0 +1,21 @@
+const elementoChute = document.querySelector('#chute')
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+   console.log(e.results[0][0].transcript)
+   exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute) {
+   elementoChute.innerHTML = `
+      <div>Você disse</div>
+      <span class="box">${chute}</span>
+   `
+}
